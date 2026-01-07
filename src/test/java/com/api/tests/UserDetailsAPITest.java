a5abe614fe4b0f0cb3034994528687e0d966c3c1
HellRaiser <qwqefs@gmail.com>
2026-01-07 14:56:11 +0530

    Created AuthTokenProvider- Manage token for different roles UserDetailsAPITest, MasterAPITest, CountAPITest.

--- src/test/java/com/api/tests/UserDetailsAPITest.java
+++ src/test/java/com/api/tests/UserDetailsAPITest.java
@@ -2,6 +2,9 @@ package com.api.tests;
 
 import static com.api.utils.ConfigManager.*;
 
+import static com.api.utils.AuthTokenProvider.*;
+
+import static com.api.constant.Role.*;
 import io.restassured.http.Header;
 import io.restassured.module.jsv.JsonSchemaValidator;
 import org.testng.annotations.Test;
@@ -17,7 +20,7 @@ public class UserDetailsAPITest {
     @Test
     public void UserDetailsAPITest() throws IOException {
 
-        Header headerOne = new Header("Authorization","eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NCwiZmlyc3RfbmFtZSI6ImZkIiwibGFzdF9uYW1lIjoiZmQiLCJsb2dpbl9pZCI6ImlhbWZkIiwibW9iaWxlX251bWJlciI6Ijg4OTk3NzY2NTUiLCJlbWFpbF9pZCI6Im1hcmtAZ21haWwuY29tIiwicGFzc3dvcmQiOiI1ZjRkY2MzYjVhYTc2NWQ2MWQ4MzI3ZGViODgyY2Y5OSIsInJlc2V0X3Bhc3N3b3JkX2RhdGUiOm51bGwsImxvY2tfc3RhdHVzIjowLCJpc19hY3RpdmUiOjEsIm1zdF9yb2xlX2lkIjo1LCJtc3Rfc2VydmljZV9sb2NhdGlvbl9pZCI6MSwiY3JlYXRlZF9hdCI6IjIwMjEtMTEtMDNUMDg6MDY6MjMuMDAwWiIsIm1vZGlmaWVkX2F0IjoiMjAyMS0xMS0wM1QwODowNjoyMy4wMDBaIiwicm9sZV9uYW1lIjoiRnJvbnREZXNrIiwic2VydmljZV9sb2NhdGlvbiI6IlNlcnZpY2UgQ2VudGVyIEEiLCJpYXQiOjE3NjYwODQ1ODV9.Dyf0F0br36ncwv8lvnlgkwMO6ytX4pxjVqTIjJjJglQ");
+        Header headerOne = new Header("Authorization", getToken(FD));
 
         String fname = given()
                 .baseUri(getProperty("BASE_URI"))
